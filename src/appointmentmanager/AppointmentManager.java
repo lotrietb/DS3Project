b9e32389693a88a0dfccb79d7af0380ab6ea3494
Clive Evertse <clive.e@uusi.co.za>
2014-06-03 11:26:21 +0200

    removed views from different packages added it to main package

--- src/appointmentmanager/AppointmentManager.java
+++ src/appointmentmanager/AppointmentManager.java
@@ -17,6 +17,12 @@ public class AppointmentManager {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+              public void run() {
+                   MainMenu frame = new MainMenu();
+                   frame.setVisible(true);
+              }
+        });
     }
     
 }
